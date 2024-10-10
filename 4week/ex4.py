a025d6683e826962ecde7d1353f671f8cb0ee49f
yoseph124 <jyp360@naver.com>
2024-10-10 12:33:01 +0900

    5주차 실습파일

--- 4week/ex4.py
+++ 4week/ex4.py
@@ -1,9 +1,13 @@
 def sumfunc(num1):
     sum1=0
-    for i in range(1,num1+1):
-        sum1=i+sum1
-    return sum1
+    if  num1 > 0:
+        for i in range(1,num1+1):
+            sum1=i+sum1
+        print(f"1~{num1}까지의 정수의 합은 {sum1}입니다.")
+        return
+    else:
+        print("1이상의 정수를 입력하세요")
+        return
 
-a=int(input("1이상의 정수를 입력하세요 : "))
-
-print(f"1~{a}까지의 정수의 합은 {sumfunc(a)}입니다.")
+a=int(input("1이상의 정수를 입력하시오 : "))
+sumfunc(a)
